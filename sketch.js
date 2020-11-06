e4305df501ca1897bf6dccd8a38ee36598df6b3b
ParthTibdewala <73227422+ParthTibdewala@users.noreply.github.com>
2020-11-06 19:54:37 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,36 @@
+const Engine=Matter.Engine
+const World=Matter.World
+const Bodies=Matter.Bodies
+
+var engine,world,object,ball
+function setup() {
+  createCanvas(400,400);
+  //createSprite(400, 200, 50, 50);
+  engine=Engine.create();
+  world=engine.world
+
+  var object_options={
+    isStatic:true
+  }
+  object=Bodies.rectangle(200,390,200,20,object_options);
+  World.add(world,object);
+  
+  var ball_options={
+    restitution:1.0
+  }
+ball=Bodies.circle(200,100,20,ball_options)
+World.add(world,ball);
+  console.log(object)
+  console.log (object.position.x)
+  console.log (object.position.y)
+}
+
+function draw() {
+  background(0);
+  Engine.update(engine)
+  rectMode(CENTER)
+  rect(object.position.x,object.position.y,50,50)
+  ellipseMode(RADIUS)
+  ellipse(ball.position.x,ball.position.y,20)
+  drawSprites();
+}
